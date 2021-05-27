6fe4cc83e5ed4305bf40288f78754c7ad1302692
std-freejia <std-freejia@gmail.com>
2021-05-27 16:45:53 +0900

    스프링 MVC - 과거의 MVC 구조 - 뷰 리졸버

--- servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -21,6 +21,10 @@ import javax.servlet.http.HttpServletResponse;
  *  Controller 인터페이스(과거에 사용했던 형태) : SimpleControllerHandlerAdapter
  *
  * * 실무에서는 99.9% @RequestMapping 방식의 컨트롤러를 사용한다. (핸들러 매핑과 핸들러 어댑터.)
+ *
+ * [스트링 부트가 자동으로 등록하는 뷰 리졸버]
+ * 스프링 빈으로 등록된 뷰를 찾음. 빈 이름으로 반환 (예: 엑셀 파일 생성 기능에 사용)
+ * JSP를 처리할 수 있는 뷰를 반환
  */
 
 @Component("/springmvc/old-controller")
@@ -28,6 +32,12 @@ public class OldController implements Controller {   // @Controller 애노테이
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
+
+        // 뷰 리졸버
+        // 1. application.properties 에 뷰 파일의 prefix, suffix 를 정의한다.
+        // 2. ModelAndView에 뷰의 논리 이름을 넘긴다.
+        // 스프링부트가 InternalResourceViewResolver라는 뷰 리졸버를 자동으로 등록하므로 1의 과정이 필요하다.
+
         return new ModelAndView("new-form");
     }
 }
